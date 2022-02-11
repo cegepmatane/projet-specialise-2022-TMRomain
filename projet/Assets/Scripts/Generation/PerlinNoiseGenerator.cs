7eecff381ea48c45967f582e31db683d3b5bc569
TMRomain <romainover51@gmail.com>
2022-02-11 13:46:27 -0500

    Script de generation de Noise Perlin #2

--- projet/Assets/Scripts/Generation/PerlinNoiseGenerator.cs
+++ projet/Assets/Scripts/Generation/PerlinNoiseGenerator.cs
@@ -1,33 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+
 
 public class PerlinNoiseGenerator : MonoBehaviour
 {
-    [SerializeField]
-    int Xsize = 25;
-    [SerializeField]
-    int Ysize= 25;
-    [SerializeField]
-    int seed = 1234;
+    // Hauteur et largeur de la generation Perlin
+    public int pixWidth = 100;
+    public int pixHeight = 100;
+    public float scale = 1.0F;
+    //Array qui contien les valeur du Noise Generer
+    private float[,] perlinArray;
 
-    [SerializeField]
-    float scale = 1.0f;
     
-    public float[,] myPerlinNoise;
 
-    void GenerateNoise(){
-        int y =0;
-        myPerlinNoise = new float[Xsize,Ysize];
-        while (y < Ysize)
+    void Start()
+    {
+        CalcNoise();
+
+    }
+    //Genere le Noise
+    void CalcNoise()
+    {
+        //Initialisation de l'array 
+        perlinArray = new float[pixWidth,pixHeight];
+
+        float y = 0.0F;
+
+        while (y < pixHeight)
         {
-            int x =0;
-            while (x < Xsize)
+            float x = 0.0F;
+            while (x < pixWidth)
             {
-                myPerlinNoise[x,y] = Mathf.PerlinNoise(x, y);
+                //Generer en fonction de la taille maximum et de la ou nous somme dans l'array 
+                float xCoord =  x / pixWidth * scale;
+                float yCoord =  y / pixHeight * scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                perlinArray[(int)x,(int)y] = sample;
+                // Debug.Log(perlinArray[(int)x,(int)y]);
                 x++;
             }
-
             y++;
         }
     }
